4e6eaba40369c79759c817ca62960c94eae0d192
Serene Leyba <development@softwavestudios.com>
2023-03-05 16:33:02 -0700

    Added buy cup of tea thing

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,8 @@ export default function Home() {
         <div className={styles.result}>Output: {result}</div>
 
         <footer class="page-footer font-small blue pt-4">
-          <div className={styles.footer}><br/><br/><a href="https://www.s0ftwave.com/" target="_blank">© SOFTWAVE 2023</a></div>
+          <div className={styles.footer}><br/><br/><a href="https://www.s0ftwave.com/" target="_blank">© SOFTWAVE 2023</a> || <a href="https://www.buymeacoffee.com/jleyba92K" target="_blank">Buy me a cup of tea</a></div>
+          
         </footer>
       </main>
     </div>
