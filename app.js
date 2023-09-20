d4443c0f721623f29865aeaa5cbbf70cc3e3ab16
Serene Leyba <development@softwavestudios.com>
2023-09-20 03:56:37 -0400

    maybe now

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const Rivet = require('@ironclad/rivet-node');
 
